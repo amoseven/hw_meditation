f0dfeac41c3e958d1b5574250aa690c90b55ba33
Amoseven <omsk.web@gmail.com>
2025-05-10 15:12:49 +0600

    страница 404

---
+++ src/pages/404Page.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container">
+    <div class="login">
+      <h1>404</h1>
+      <router-link to="/main">На главную</router-link>
+    </div>
+  </div>
+
+
+</template>
+
+<style scoped>
+.container {
+  display: grid;
+  place-items: center;
+  height: 100vh;
+}
+.login {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  h1 {
+    color: #f7f3f0;
+    font-size: 120px;
+    font-family: var(--font-name-secondary), sans-serif;
+  }
+
+  a {
+    color: #F7F3F0;
+    font-size: 20px;
+    text-decoration: none;
+  }
+}
+</style>
--- src/router/router.ts
+++ src/router/router.ts
@@ -3,18 +3,27 @@ import { createRouter, createWebHistory } from 'vue-router'
 export const router = createRouter({
   routes: [
     {
+      path: '/:pathMatch(.*)*',
+      component: () => import('@/pages/404Page.vue'),
+      name: 'notFound',
+    },
+    {
       path: '/',
       component: () => import('@/pages/AuthPage.vue'),
-      name: "auth"
+      name: 'auth',
     },
     {
       path: '/main',
       component: () => import('@/pages/MainView.vue'),
       children: [
-        { path: '', component: () => import('@/components/MeditationCards.vue'), name: 'meditations' },
+        {
+          path: '',
+          component: () => import('@/components/MeditationCards.vue'),
+          name: 'meditations',
+        },
         { path: 'stat', component: () => import('@/pages/StatView.vue'), name: 'stat' },
-      ]
-    }
+      ],
+    },
   ],
-  history: createWebHistory()
+  history: createWebHistory(),
 })
